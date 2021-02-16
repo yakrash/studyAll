4f053583287deb972b17ad5553233a969e6d2f88
yakrash <kraash@bk.ru>
2021-02-16 17:15:42 +0300

    Coffee_machine stage 3/6

--- src/su/bzz/study/tasks/Coffee_machine.java
+++ src/su/bzz/study/tasks/Coffee_machine.java
@@ -8,15 +8,37 @@ public class Coffee_machine {
         int water;
         int milk;
         int beans;
+        int result = 0;
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Write how many cups of coffee you will need:");
+        System.out.println("Write how many ml of water the coffee machine has:");
+        water = sc.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        milk = sc.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        beans = sc.nextInt();
+        System.out.println("Write how many cups of coffee you will need: ");
         count = sc.nextInt();
-        water = count * 200;
-        milk = count * 50;
-        beans = count * 15;
-        System.out.println("For " + count + " cups of coffee you will need:");
-        System.out.println(water + " ml of water");
-        System.out.println(milk + " ml of milk");
-        System.out.println(beans + " g of coffee beans");
+
+        if (water < 200 || milk < 50 || beans < 15) {
+            System.out.println("No, I can make only 0 cup(s) of coffee");
+        }
+        while (water >= 200 && milk >= 50 && beans >= 15 ) {
+            result++;
+            water -= 200;
+            milk -= 50;
+            beans -= 15;
+        }
+
+        if (result < count) {
+            System.out.println("No, I can make only " + result + " cup(s) of coffee");
+        }
+        if (result == count) {
+            System.out.println("Yes, I can make that amount of coffee");
+        }
+        if (result > count) {
+            System.out.println("Yes, I can make that amount of coffee (and even " + (result - count)
+                    + " more than that)");
+        }
     }
 }
